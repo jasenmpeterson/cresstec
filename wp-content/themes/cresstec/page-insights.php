54406c3e072c2eeb047eccb3e913fb0c5020d3f0
Jasen Peterson <jasen@ellcreative.com>
2018-06-18 16:19:58 -0500

    Humble beginnings of the insights page

---
+++ wp-content/themes/cresstec/page-insights.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+	<div class="insights--wrapper">
+		<?php
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post();
+					get_template_part( 'template-parts/pages/insights/content' );
+				}
+			}
+		?>
+	</div>
+<?php get_footer(); ?>
---
+++ wp-content/themes/cresstec/template-parts/pages/insights/content.php
@@ -0,0 +1,75 @@
+<div class="columns">
+	<div class="column is-1">
+		<svg class="newspaper--icon" xmlns="http://www.w3.org/2000/svg" viewBox="6938.852 226.947 66.859 66.825">
+			<g id="Group_133" data-name="Group 133" transform="translate(6736)">
+				<g id="Group_89" data-name="Group 89" transform="translate(202.852 226.947)">
+					<path id="Path_200" data-name="Path 200" class="cls-1" d="M65.818,33.4H64.811A31.375,31.375,0,0,1,11.244,55.581,31.363,31.363,0,1,1,55.581,11.21a31.3,31.3,0,0,1,9.2,22.186h2.047A33.392,33.392,0,0,0,9.8,9.8,33.413,33.413,0,0,0,33.429,66.825,33.461,33.461,0,0,0,66.859,33.4h-1.04Z" transform="translate(0 0)"/>
+				</g>
+				<g id="Group_121" data-name="Group 121" transform="translate(218.889 245.037)">
+					<path id="Path_228" data-name="Path 228" class="cls-1" d="M33.832,2.783a.9.9,0,0,0-.9.9V27.563a4.574,4.574,0,0,1-4.573,4.573H6.373A4.574,4.574,0,0,1,1.8,27.563V1.8H28.134V24.967a.9.9,0,1,0,1.8,0V.9a.9.9,0,0,0-.9-.9H.9A.9.9,0,0,0,0,.9V27.553a6.379,6.379,0,0,0,6.373,6.373H28.35a6.379,6.379,0,0,0,6.373-6.373V3.683a.892.892,0,0,0-.89-.9Zm0,0"/>
+					<path id="Path_229" data-name="Path 229" class="cls-1" d="M65.106,57.2a.9.9,0,0,0,0-1.8H45.5a.9.9,0,1,0,0,1.8Zm0,0" transform="translate(-40.42 -50.208)"/>
+					<path id="Path_230" data-name="Path 230" class="cls-1" d="M65.106,251H45.5a.9.9,0,0,0,0,1.8H65.106a.9.9,0,0,0,0-1.8Zm0,0" transform="translate(-40.42 -227.477)"/>
+					<path id="Path_231" data-name="Path 231" class="cls-1" d="M95.194,300.2H78.7a.9.9,0,0,0,0,1.8H95.2a.9.9,0,0,0,.9-.9.92.92,0,0,0-.909-.9Zm0,0" transform="translate(-70.509 -272.066)"/>
+					<path id="Path_232" data-name="Path 232" class="cls-1" d="M41.4,99.4v9.728a.9.9,0,0,0,.9.9h9.765a.9.9,0,0,0,.9-.9V99.4a.9.9,0,0,0-.9-.9H42.3a.9.9,0,0,0-.9.9Zm1.8.9h7.966v7.929H43.2V100.3Zm0,0" transform="translate(-37.52 -89.269)"/>
+					<path id="Path_233" data-name="Path 233" class="cls-1" d="M197.366,108.1H190.9a.9.9,0,0,0,0,1.8h6.467a.9.9,0,1,0,0-1.8Zm0,0" transform="translate(-172.194 -97.969)"/>
+					<path id="Path_234" data-name="Path 234" class="cls-1" d="M197.366,151.4H190.9a.9.9,0,0,0,0,1.8h6.467a.9.9,0,1,0,0-1.8Zm0,0" transform="translate(-172.194 -137.211)"/>
+					<path id="Path_235" data-name="Path 235" class="cls-1" d="M198.266,198.4a.9.9,0,0,0-.9-.9H190.9a.9.9,0,1,0,0,1.8h6.467a.9.9,0,0,0,.9-.9Zm0,0" transform="translate(-172.194 -178.991)"/>
+				</g>
+			</g>
+		</svg>
+		<svg class="circular--accents--dark" xmlns="http://www.w3.org/2000/svg" viewBox="6906.891 199 139.902 111.922">
+			<g id="Group_135" data-name="Group 135" transform="translate(6736)">
+				<g id="Group_134" data-name="Group 134">
+					<ellipse id="Ellipse_136" data-name="Ellipse 136" class="cls-1" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(179.014 199)"/>
+					<ellipse id="Ellipse_137" data-name="Ellipse 137" class="cls-2" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(271.078 293.772)"/>
+					<ellipse id="Ellipse_141" data-name="Ellipse 141" class="cls-2" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(212.41 209.831)"/>
+					<ellipse id="Ellipse_138" data-name="Ellipse 138" class="cls-3" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(260.247 214.344)"/>
+					<ellipse id="Ellipse_139" data-name="Ellipse 139" class="cls-4" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(170.891 252.253)"/>
+					<ellipse id="Ellipse_140" data-name="Ellipse 140" class="cls-4" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(292.741 273.013)"/>
+					<ellipse id="Ellipse_142" data-name="Ellipse 142" class="cls-4" cx="4.062" cy="4.062" rx="4.062" ry="4.062" transform="translate(220.534 302.798)"/>
+					<g id="Group_77" data-name="Group 77" transform="translate(230.462 207.123)">
+						<line id="Line_200" data-name="Line 200" class="cls-5" y2="6.318" transform="translate(3.159)"/>
+						<line id="Line_201" data-name="Line 201" class="cls-5" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_83" data-name="Group 83" transform="translate(269.274 276.623)">
+						<line id="Line_200-2" data-name="Line 200" class="cls-5" y2="6.318" transform="translate(3.159 0)"/>
+						<line id="Line_201-2" data-name="Line 201" class="cls-5" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_81" data-name="Group 81" transform="translate(272.884 237.812)">
+						<line id="Line_200-3" data-name="Line 200" class="cls-6" y2="6.318" transform="translate(3.159)"/>
+						<line id="Line_201-3" data-name="Line 201" class="cls-6" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_78" data-name="Group 78" transform="translate(200.676 299.188)">
+						<line id="Line_200-4" data-name="Line 200" class="cls-5" y2="6.318" transform="translate(3.159 0)"/>
+						<line id="Line_201-4" data-name="Line 201" class="cls-5" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_84" data-name="Group 84" transform="translate(267.473 295.408)">
+						<line id="Line_200-5" data-name="Line 200" class="cls-5" y2="4.641" transform="translate(2.321 0)"/>
+						<line id="Line_201-5" data-name="Line 201" class="cls-5" x1="4.641" transform="translate(0 2.321)"/>
+					</g>
+					<g id="Group_85" data-name="Group 85" transform="translate(304.475 237.812)">
+						<line id="Line_200-6" data-name="Line 200" class="cls-6" y2="6.318" transform="translate(3.159)"/>
+						<line id="Line_201-6" data-name="Line 201" class="cls-6" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_79" data-name="Group 79" transform="translate(279.202 260.376)">
+						<line id="Line_200-7" data-name="Line 200" class="cls-6" y2="6.318" transform="translate(3.159)"/>
+						<line id="Line_201-7" data-name="Line 201" class="cls-6" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_80" data-name="Group 80" transform="translate(187.137 235.104)">
+						<line id="Line_200-8" data-name="Line 200" class="cls-5" y2="6.318" transform="translate(3.159 0)"/>
+						<line id="Line_201-8" data-name="Line 201" class="cls-5" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+					<g id="Group_82" data-name="Group 82" transform="translate(190.748 275.721)">
+						<line id="Line_200-9" data-name="Line 200" class="cls-5" y2="6.318" transform="translate(3.159)"/>
+						<line id="Line_201-9" data-name="Line 201" class="cls-5" x1="6.318" transform="translate(0 3.159)"/>
+					</g>
+				</g>
+			</g>
+		</svg>
+
+	</div>
+	<div class="column is-6">
+		<h1><?php the_title(); ?></h1>
+        <?php the_content(); ?>
+	</div>
+</div>
